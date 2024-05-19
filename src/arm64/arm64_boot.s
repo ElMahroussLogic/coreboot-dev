08aab823c2dbcef032fb318acee13398a1aeb993
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-19 09:16:45 +0200

    arm64_boot.s: fix mv with mov.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- src/arm64/arm64_boot.s
+++ src/arm64/arm64_boot.s
@@ -4,12 +4,13 @@
 
 ------------------------------------------- */
 
+.text
+
 .balign 4
 .global __start
  
 __start
-	mv sp, __mp_stack_end
-
+	mov sp, __mp_stack_end
 	/* don't care about page_zero, it's gonna be a raw binary */
-
     b mp_start_exec
+    
