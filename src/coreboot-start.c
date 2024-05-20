f1aa9af05c40c74b297852247daf9a52e0a891bc
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-20 10:39:19 +0200

    Use Linux/macOS toolchain.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- src/coreboot-start.c
+++ src/coreboot-start.c
@@ -46,7 +46,7 @@ void mp_start_exec(void)
 	// let the hart 0 init our stuff.
 	if (hart == 1)
 	{
-		mp_put_string(">> Welcome to CoreBoot, (c) Mahrouss-logic. Built the ");
+		mp_put_string(">> Welcome to CoreBoot, (c) SoftwareLabs. Built the ");
 		mp_put_string(__DATE__);
 		mp_put_string("\r\r\n");
 
