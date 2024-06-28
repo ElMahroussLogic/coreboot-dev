f28654951f96ba2b47ce87ecf59f3385502ea9ed
Amlal EL Mahrouss <amlalelmahrouss@icloud.com>
2024-06-28 07:43:22 +0200

    WIP: Flash memory driver.
    Signed-off-by: Amlal EL Mahrouss <amlalelmahrouss@icloud.com>

---
+++ src/custom_hw/coreboot-flash.c
@@ -0,0 +1,11 @@
+/* -------------------------------------------
+
+Copyright Zeta Electronics Corporation
+
+------------------------------------------- */
+
+#include <lib/mp-bit.h>
+#include <lib/partition-map.h>
+#include <lib/pci-tree.h>
+#include <lib/boot.h>
+
