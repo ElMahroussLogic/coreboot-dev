0688c80340af4e2c1d16ee79f47b32bf236067c8
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-11-07 16:41:53 +0100

    Making my CV better.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,8 @@
 # CoreBoot
 ## Multi-Platform IPL.
 
-Author: EL Mahrouss Logic
+Author: Amlal EL Mahrouss
 </br>
 Purpose: A firmware for 64x0, RISC-V, AMD64 and POWER architectures.
 
-###### Copyright 2024, EL Mahrouss Logic. all rights reserved.
+###### Copyright 2024, Amlal EL Mahrouss. all rights reserved.
--- src/coreboot-ahci-driver.c
+++ src/coreboot-ahci-driver.c
@@ -1,17 +1,17 @@
 /* -------------------------------------------
 
-    Copyright (C) 2024, EL Mahrouss Logic, all rights reserved.
+    Copyright (C) 2024, Amlal EL Mahrouss, all rights reserved.
 
 ------------------------------------------- */
 
 /**
  * @file coreboot-ahci-driver.cc
- * @author EL Mahrouss Logic (amlal@el-mahrouss-logic.com)
+ * @author Amlal EL Mahrouss (amlal@el-mahrouss-logic.com)
  * @brief PowerPC Disk support, via AHCI.
  * @version 0.2
  * @date 2024-01-16
  *
- * @copyright Copyright (c) 2024, EL Mahrouss Logic..
+ * @copyright Copyright (c) 2024, Amlal EL Mahrouss..
  *
  */
 
